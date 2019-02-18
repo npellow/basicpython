8f4bd134826dc52bbbdc9271a321dff45587a560
Nichole Pellow <ladypellow@gmail.com>
2019-02-18 13:22:48 -0500

    Added a practice file
    Python practice using input and print to create a very simple madlib.

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+# noun = input("Please enter a noun: ")
+# print("You are as tall as a", noun)
+
+# Above is an example for making a madlibs that
+# prints to the console using Python!
+
+color = input("Please enter a color:  ")
+animal = input("Please enter an animal:  ")
+name = input("Please enter a name:  ")
+place = input("Please enter an environment type:  ")
+mood = input("Please enter a mood:  ")
+size = input("Please enter a size:  ")
+gem_color = input("Please enter a color:  ")
+
+
+
+print("\nThere once was a", color, animal, "named", name, "that lived in the", place, "far away.")
+print(name, "was feeling", mood, "one day when they decided to go on an adventure.")
+print("During the adventure,", name, "found a cave full of", size, gem_color, "crystals.")
+print(name, "was pleased by the numerous", size, "crystals, and decided to stay in the cave forever.")
+print("\nThe End")
